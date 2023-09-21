d5f6703b989b645db99cb774176eeb5a3e345238
Sean127 <sdoran401@gmail.com>
2023-09-21 14:43:00 +0000

    Add changes to Product model

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254, null=True, blank=True)
-
+    
     def __str__(self):
         return self.name
 
@@ -12,8 +12,25 @@ class Category(models.Model):
         return self.display_name
 
 
+class Platform(models.Model):
+    platform = [
+        ("360", "Xbox 360"),
+        ("PS3", "Playstaion 3"),
+    ]
+
+
+class Type(models.Model):
+    product_type = [
+        ("game", "Game"),
+        ("console", "Console"),
+        ("accessories", "Game"),
+    ]
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    platform = models.ForeignKey('Platform', null=True, blank=True, on_delete=models.SET_NULL)
+    product_type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
