7529da705747fb0151863502dce5c8ab2f6fccf6
Sean127 <sdoran401@gmail.com>
2023-09-26 23:58:59 +0000

    Added add to bag functionality

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254, null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
 
@@ -20,7 +20,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
-    no_of_reviews = models.IntegerField(null=True,blank=True)
+    no_of_reviews = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
